3b8725207ba1723c9ff7352d3fbbbe1630f123b2
Jose Villa <j0542420@amarillocollege.com>
2025-03-22 18:36:10 -0500

    finish

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;  // Import the Scanner class
+
+class Main {
+  public static void main(String[] args) {
+    try (Scanner myObj = new Scanner(System.in)) {
+        //declarations
+
+        //input
+        double length = 0;
+        double width = 0;
+
+        //output
+        double area = 0;
+        double perimeter = 0;
+
+        //user input the length of the rectangle
+        System.out.println("Enter the length of the rectangle: ");
+        //collect the user input for the length
+        length = myObj.nextDouble();
+
+        //user input the width of the rectangle
+        System.out.println("Enter the width of the rectangle: ");
+        //collect the user input for the width
+        width = myObj.nextDouble();
+
+        //calculate the area and perimeter of the rectangle
+        area = length * width;
+        perimeter = 2 * (length + width);
+
+        //outputs the results for the area and perimeter
+        System.out.println("Area of the rectangle: " + area );
+        System.out.println("Perimeter of the rectangle: " + perimeter);
+
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input
+    }
+  }
+}
